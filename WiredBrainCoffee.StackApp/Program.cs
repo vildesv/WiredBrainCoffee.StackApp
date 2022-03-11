4c38d20c04d8586ad45ac9dc23ce5b9d2d62ebd5
Vilde Svenkesen <vilde.svenkesen@imatis.com>
2022-03-11 10:04:42 +0100

    Add project files.

---
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;   // to use the Stack<T> class of .NET instead of the SimpleStack<T> class.
+
+namespace WiredBrainCoffee.StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackDoubles();
+            StackStrings();
+
+            Console.ReadLine();
+        }
+
+        private static void StackDoubles()
+        {
+            var stack = new Stack<double>();  // SimpleStack class
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        private static void StackStrings()
+        {
+            var stack = new Stack<string>(); // SimpleStack class
+            stack.Push("Wired Brain Coffee");
+            stack.Push("Pluralsight");
+
+            while(stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+
+    }
+
+}
+
+
+/* 
+
+Code from a Pluralsight course on C# Generics:
+(https://app.pluralsight.com/library/courses/c-sharp-generics/table-of-contents)
+
+*** Part 1. Course overview ***
+
+    - The need for generics
+    - Implementing generic classes
+    - Create generic interfaces
+    - Writing generic methods
+    - Using generic delegates
+
+What you will learn: 
+How to use C# generics in your .NET applications to write type-safe, reusable, and performant code.
+
+*** Part 2. Understanding the need for Generics ***
+
+    - Implement a SimpleStack class that works as a storage for any type
+        * Use the object type
+        * Copy and paste for every type
+        * Create a generic SimpleStack<T> class (SimpleStack of T class)
+        
+    - Advantages of generics
+        * Code reuse
+        * Type-safety
+        * Performance
+
+    - Use the Stack<T> class of .NET
+
+ */
---
+++ WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,17 @@
+﻿namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStack<T>  // T or TypePlaceholder is a so-called generic type parameter 
+    {
+        private T[] _items;
+        private int _currentIndex = -1;
+
+        public SimpleStack() => _items = new T[10];        // => - expression body for the constuctor (without curly breackets)
+
+        public int Count => _currentIndex + 1;
+
+        public void Push(T item) => _items[++_currentIndex] = item;
+        //  { _currentIndex += 1; _items[++_currentIndex] = item; }
+    
+        public T Pop() => _items[_currentIndex--];
+    }
+}
